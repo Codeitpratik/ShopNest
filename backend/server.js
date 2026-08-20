5c502392eee2ca741a0a009731c7836c83f64e86
Pratik <kashyappratik06@gmail.com>
2026-08-20 17:56:28 +0530

    fix cors for frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const app = express();
 
 // Set CORS for frontend URL / allow single-node deploy
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL],
+  origin: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://shopnest-frontend-cne6.onrender.com'
+  ],
   credentials: true
 }));
 
